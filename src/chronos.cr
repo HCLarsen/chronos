e945f9e5cae6c83e8ae9a2b72eae2f0ab69765d2
Chris Larsen <clarsendev@protonmail.com>
2021-12-19 23:25:15 -0500

    Add Chronos#in method

--- src/chronos.cr
+++ src/chronos.cr
@@ -2,5 +2,23 @@
 class Chronos
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
-class
+  property location : Time::Location
+  getter tasks = [] of Task
+
+  def initialize(@location = Time::Location.local)
+  end
+
+  def in(span : Time::Span, &block)
+    run_time = Time.local + span
+    @tasks << OneTimeTask.new(run_time, &block)
+  end
+
+  def run
+    spawn do
+      first_task = @tasks.first
+      wait = first_task.next_run - Time.local
+      sleep wait
+      @tasks.first.run
+    end
+  end
+end
---
+++ test/chronos_test.cr
@@ -0,0 +1,31 @@
+require "minitest/autorun"
+
+require "/../src/chronos"
+
+class ChronosTest < Minitest::Test
+  def test_initializes_chronos
+    scheduler = Chronos.new
+
+    assert_equal 0, scheduler.tasks.size
+    assert_equal Time::Location.local, scheduler.location
+  end
+
+  def test_adds_one_time_task
+    @test_val = 0
+    scheduler = Chronos.new
+
+    scheduler.in(3.milliseconds) do
+      @test_val = 5
+    end
+
+    task = scheduler.tasks.first
+    assert_equal Chronos::OneTimeTask, task.class
+    scheduler.run
+
+    sleep 2.milliseconds
+    assert_equal 0, @test_val
+
+    sleep 4.milliseconds
+    assert_equal 5, @test_val
+  end
+end
